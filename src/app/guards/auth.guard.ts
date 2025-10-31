372c2c00b8325f5a165232199c765675fc89489f
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2025-10-31 15:08:45 +0100

    Refresh Token realizado.
    -Refresh Token
    - Solo el admin puede entrar a la web

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -9,11 +9,19 @@ export class authGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
+    if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/login']);
       return false;
     }
+
+    const rol = this.authService.getUserRole();
+
+    if (rol === 'admin') {
+      return true;
+    }
+
+    // Si no es admin, redirigimos a otra página (por ejemplo home)
+    this.router.navigate(['/']);
+    return false;
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
+import {jwtDecode} from 'jwt-decode';
+
 
 export interface User {
   _id: string;
@@ -54,6 +56,7 @@ export class AuthService {
     localStorage.removeItem('token');
     localStorage.removeItem('refreshToken');
     this.currentUserSubject.next(null);
+    
   }
 
   getCurrentUser(): User | null {
@@ -67,8 +70,30 @@ export class AuthService {
     return localStorage.getItem('token');
   }
 
+  getUserRole(): string | null {
+    const token = this.getToken();
+    if (!token) {
+      return null;
+    }
+    try {
+    const decoded: any = jwtDecode(token);
+    return decoded.payload?.rol || null;
+  } catch (error) {
+    console.error('Error al decodificar token', error);
+    return null;
+  }
+  }
   // Método para crear admin (solo desarrollo)
   createAdminUser(): Observable<any> {
     return this.http.post(`${this.apiUrl}/user/auth/create-admin`, {});
+  }
+  refreshToken(): Observable<any> {
+    const refreshToken = localStorage.getItem('refreshToken');
+    const currentUser = localStorage.getItem('currentUser');
+    if (!refreshToken || !currentUser) {
+      throw new Error('No refresh token or current user found');
+    }
+    const user = JSON.parse(currentUser);
+    return this.http.post(`${this.apiUrl}/user/refresh`, { refreshToken, userId: user._id });
   }
 }
