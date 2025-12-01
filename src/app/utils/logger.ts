5c5d899bdd7a5259be923e8e7359f27e4e67d93a
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2025-12-01 11:17:59 +0100

    Logs eliminados

--- src/app/utils/logger.ts
+++ src/app/utils/logger.ts
@@ -1,3 +1,7 @@
+import { environment } from "../environments/environment";
+
+const noop = () => {};
+
 export const logger = {
   log: (...args: any[]) => { try { console.log(...args); } catch {} },
   info: (...args: any[]) => { try { console.info(...args); } catch {} },
@@ -5,5 +9,15 @@ export const logger = {
   error: (...args: any[]) => { try { console.error(...args); } catch {} },
   debug: (...args: any[]) => { try { console.debug(...args); } catch {} }
 };
+
+if (environment.production) {
+  logger.log = noop;
+  logger.info = noop;
+  logger.debug = noop;
+
+  // Si quieres silenciar TODO, descomenta las siguientes líneas:
+  // logger.warn = noop;
+  // logger.error = noop;
+}
 
 export default logger;
